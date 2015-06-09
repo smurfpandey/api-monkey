aa2e165c0dac72e016e91a23a5ee1d27e1ff1aab
migomhmi <migom.08015020@gmail.com>
2015-06-10 01:10:27 +0530

    UI ready & working

---
+++ APIMonkey/MonkeyUI/MonkeyGet.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MonkeyGet.aspx.cs" Inherits="APIMonkey.MonkeyUI.MonkeyGet" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/jsoneditor.css" rel="stylesheet" />
+    <link href="css/MonkeyCSS.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="col-lg-12">
+            <div class="page-header">
+                GET YOUR BANANA
+            </div>
+            <div>
+                <input id="urlSeg" class="monkey-url-ip" type="text" placeholder="Your Url" />
+            </div>
+            <div style="margin-top: 20px">
+                <input id="getBanana" type="button" class="btn btn-primary" value="GET" />
+            </div>
+            <div id="divJsonEditor" style="margin-top: 20px">
+                <div id="jsoneditor" style="height: 700px">
+                </div>
+            </div>
+        </div>
+    </form>
+    <script src="Script/jquery-1.11.3.min.js"></script>
+    <script src="Script/jsoneditor.js"></script>
+    <script src="Script/monkey-api.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            var objMonkey = new MonkeyGetApi();
+            objMonkey.init();
+        });
+    </script>
+</body>
+</html>
---
+++ APIMonkey/MonkeyUI/Script/monkey-api.js
@@ -0,0 +1,125 @@
+﻿
+var MonkeyApi = function () {
+    var editor;
+    function _initiateJsonEditor() {
+
+        // create the editor
+        var options = {
+            "mode": "code",
+            "indentation": 2
+        };
+        var container = document.getElementById("jsoneditor");
+        editor = new JSONEditor(container, options);
+    }
+
+    function _bindEvent() {
+
+        $('#btnBananaSubmit').on('click', function () {
+            //validate json
+            try {
+                //on json valid
+                var json = editor.get();
+                var stringJson = JSON.stringify(json);
+                $.ajax({
+                    url: '/banana',
+                    type: 'PUT',
+                    contentType: "application/json; charset=utf-8",
+                    data: stringJson,
+                    success: function (data, textStatus, request) {
+                        var bananaLocation = request.getResponseHeader('Location');
+
+                        $('.monkey-banana-loc').append(bananaLocation);
+
+                        $('.banana-loc-div-container').removeClass('hide');
+
+                        $('.editor-container').addClass('hide');
+                    },
+                    error: function () {
+                        $('.monkey-banana-loc').append('Opps! Some error in posting your json. Please try again');
+
+                        $('.banana-loc-div-container').removeClass('hide');
+
+                        $('.editor-container').addClass('hide');
+                    }
+                });
+            }
+            catch (e) {
+                //on json invalid
+                alert("invalid json");
+            }
+        });
+
+        $('#btnAnotherBanana').on('click', function () {
+            $('#jsoneditor').html('');
+            _initiateJsonEditor();
+
+            $('.banana-loc-div-container').addClass('hide');
+
+            $('.editor-container').removeClass('hide');
+        });
+    }
+
+    return {
+        init: function () {
+            _initiateJsonEditor();
+            _bindEvent();
+        }
+    }
+}
+
+var MonkeyGetApi = function () {
+
+    var editor;
+    function _initiateJsonEditor() {
+
+        // create the editor
+        var options = {
+            "mode": "code",
+            "indentation": 2
+        };
+        var container = document.getElementById("jsoneditor");
+        editor = new JSONEditor(container, options);
+    }
+
+    function _bindEvent() {
+
+        $('#getBanana').on('click', function () {
+            //validate json
+            try {
+                var urlSeg = $('#urlSeg').val();
+                //var qwe = url.segment("2");
+                var pathname = urlSeg.split("/");
+                var filename = pathname[pathname.length - 1];
+                $.ajax({
+
+                    url: urlSeg,
+
+                    type: 'GET',
+                    contentType: "application/json; charset=utf-8",
+                    data: { bananaId: filename },
+                    success: function (respData) {      
+                            editor.set(respData);
+                    },
+                    error: function (xhr, textStatus) {
+                        if (xhr.status === 404) {
+                            alert("Opps! No banana found in the specified location!");
+                        }
+                    }
+                });
+            }
+            catch (e) {
+                //on json invalid
+                alert("invalid json");
+            }
+        });
+
+
+    };
+
+    return {
+        init: function () {
+            _initiateJsonEditor();
+            _bindEvent();
+        }
+    }
+}
